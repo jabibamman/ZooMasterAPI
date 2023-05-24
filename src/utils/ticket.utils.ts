9f096b5d4daca31a9cbc3e7d0372cca2b4977cce
Ronan Kielt <rkielt@myges.fr>
2023-05-24 19:35:30 +0200

    feat: classes for every pass

--- src/utils/ticket.utils.ts
+++ src/utils/ticket.utils.ts
@@ -3,10 +3,10 @@ import {Ticket} from "../models/ticket.model";
 export enum Pass {
     PASS_DAY = "PASS_DAY",
     PASS_WEEKEND = "PASS_WEEKEND",
-    PASS_YEAR = "annuel",
-    PASS_DAYMONTH = "un jour par mois toute l'année",
-    PASS_ESCAPEGAME = "escape game",
-    PASS_NIGHT = "night"
+    PASS_YEAR = "PASS_YEAR",
+    PASS_DAYMONTH = "PASS_DAYMONTH",
+    PASS_ESCAPEGAME = "PASS_ESCAPEGAME",
+    PASS_NIGHT = "PASS_NIGHT"
 }
 
 export class PassDay implements Ticket {
@@ -17,7 +17,7 @@ export class PassDay implements Ticket {
     constructor(start: Date) {
         this.name = Pass.PASS_DAY;
         this.start = start;
-        this.expiration = start.getDay()+1;
+        this.expiration = new Date(Date.now());
     }
 }
 
@@ -29,7 +29,7 @@ export class PassWeek implements Ticket {
     constructor(start: Date) {
         this.name = Pass.PASS_WEEKEND;
         this.start = start;
-        this.expiration = start.getDay() + 1;
+        this.expiration = new Date(start.getFullYear(), start.getMonth(), start.getDay() + 1);
     }
 }
 
@@ -40,6 +40,33 @@ export class PassYear implements Ticket {
 
     constructor(year: number) {
         this.name = Pass.PASS_YEAR;
-        this.start = new Date(year, Date.now())
+        this.start = new Date(year, Date.now());
+        this.expiration = new Date(year + 1, Date.now());
+    }
+}
+
+export class PassDayMonth implements Ticket {
+    expiration: Date;
+    name: Pass;
+    start: Date;
+
+    constructor(year: number) {
+        this.name = Pass.PASS_DAYMONTH;
+        this.start = new Date(year, Date.now());
+        this.expiration = new Date(year + 1, Date.now());
+    }
+}
+
+export class PassEscapeGame extends PassDay {
+    constructor(start: Date) {
+        super(start);
+        this.name = Pass.PASS_ESCAPEGAME;
+    }
+}
+
+export class PassNight extends PassDay {
+    constructor(start: Date) {
+        super(start);
+        this.name = Pass.PASS_NIGHT;
     }
 }
